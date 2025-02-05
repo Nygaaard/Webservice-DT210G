0fc3c14ca85ed8d24c0b3a5e0752c6b338de676c
Nygaaard <nygardandreas@hotmail.com>
2025-02-05 16:45:14 +0100

    Initialized project. Created install file

---
+++ install.js
@@ -0,0 +1,26 @@
+//Installerings-script för todo-sida
+//Av Andreas Nygård
+
+//Hämta Sqlite3
+const sqlite3 = require("sqlite3").verbose();
+
+//Skapa ny databas
+const db = new sqlite3.Database("./db/todoList.db");
+
+//Skapa tabell för databas
+db.serialize(() => {
+    db.run("DROP TABLE if EXISTS todos");
+
+    //Tabell "todos"
+    db.run(`
+            CREATE TABLE todos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEST NOT NULL,
+                description TEXT NOT NULL,
+                status TEXT DEFAULT 'ej påbörjad' NOT NULL,
+                posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+            )
+        `)
+})
+
+db.close();
